0cccd05b1125c912bad5c7f2d8e521e32e1a0462
Alexey Strakh <alexey.strakh@microsoft.com>
2021-03-29 16:58:10 -0700

    add new item - in progress

--- tabs/src/components/Tab.tsx
+++ tabs/src/components/Tab.tsx
@@ -5,13 +5,26 @@ import React from 'react';
 import './App.css';
 import './Tab.css';
 import { MODS } from "mods-client";
-import { Checkbox } from '@fluentui/react-northstar';
+import { Text, Input, Button, Checkbox, AddIcon, NotesIcon } from '@fluentui/react-northstar';
+
+const styles = {
+  ToDoListItemNew: {
+
+  },
+  ToDoListItemCompleted: {
+    color: 'gray',
+    textDecoration : 'line-through'
+  },
+  ToDoListItemNotCompleted: {
+
+  }
+}
 
 /**
  * The 'PersonalTab' component renders the main tab content
  * of your app.
  */
-class Tab extends React.Component {
+class Tab extends React.Component<any> {
 
   constructor(props: any) {
     super(props)
@@ -38,26 +51,68 @@ class Tab extends React.Component {
     });
   }
 
+  addNewTask = () => {
+    this.setStage({addNewTask: Date.now() });
+    console.log('NOW:', this.state.addNewTask);
+  }
+
   render() {
 
-    var data = [
-      {"name": "Add a task", isCompleted: false, isNew: true },
-      {"name": "Task 1", isCompleted: false },
-      {"name": "Task 2", isCompleted: false },
-      {"name": "Task 3", isCompleted: true }
+    var todoItems = [
+      { "name": "Add a task", isCompleted: false, isNew: true },
+      { "name": "Task 1", isCompleted: false },
+      { "name": "Task 2", isCompleted: false },
+      { "name": "Task 3", isCompleted: true }
     ];
+
+    // TODO: fix context
+    // let context = this;
 
     return (
       <div className="Tab">
         <div className="Title">ToDo App</div>
         <div className="Subtitle">Hello, {this.state.userInfo.userName}</div>
         <div className="ToDoList">
-          {data.map(function(d, index){
-            return (
-              <li className="ToDoListItem" key={index}><Checkbox />{d.name}</li>
-            )
-          })}
-      </div>
+          {
+            todoItems.map(function (todoItem, index) {
+              if (todoItem.isNew) {
+                return (
+                  <li className="ToDoListItem" key={index}>
+                    <Input 
+                      placeholder={todoItem.name} 
+                      clearable 
+                      icon={<AddIcon />} 
+                      iconPosition="start" 
+                      // fluid
+                      styles={styles.ToDoListItemNew}
+                      input={{
+                        styles:{
+                          background: 'transparent',
+                        }
+                      }}>
+                      </Input>
+                      <Button 
+                        icon={<NotesIcon />} 
+                        text
+                        iconOnly
+                        onClick={this.addNewTask}
+                      >
+                      </Button>
+                  </li>
+                )
+              } else {
+                return (
+                  <li className="ToDoListItem" key={index}>
+                    <Checkbox checked={todoItem.isCompleted} />
+                    <Text 
+                      styles={todoItem.isCompleted ? styles.ToDoListItemCompleted : styles.ToDoListItemNotCompleted}>{todoItem.name}
+                    </Text>
+                  </li>
+                )
+              }
+            })
+          }
+        </div>
       </div>
     );
   }
